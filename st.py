bcc294d1204029c775cece44ea562b692d0c9fb9
Bhanu Kumar <84540892+bk790@users.noreply.github.com>
2024-05-02 11:28:00 +0530

    Update st.py

--- st.py
+++ st.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import joblib
+model = joblib.load('news-classification-model.pkl')
 
-model=joblib.load('news-classification-model.pkl')
-news_labels={0:'tech', 1:'business', 2:'sport', 3:'entertainment', 4:'politics'}
-st.title("news Classification")
-user_input=st.text_area("Enter news here:")
-if st.button("predict"):
-    predicted_label=model.predict([user_input])[0]
-    predicted_news_label=news_labels([str(predicted_label)])
-    print(predicted_news_label)
+# Define sentiment labels
+news_labels = {0: 'tech', 1: 'business', 2: 'sport', 3: 'entertainment', 4: 'politics'}
+
+
+# Create Streamlit app
+st.title("News Classification")
+
+# Input text area
+user_input = st.text_area('Enter news here')
+
+# Prediction button
+if st.button("Classify"):
+    predicted_label = model.predict([user_input])[0]
+
+    predicted_news_label = news_labels[predicted_label]
+
+    # Display classification
+    st.info(f"Predicted News Category: {predicted_news_label}")
+
